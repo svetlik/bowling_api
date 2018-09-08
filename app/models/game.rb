b5731065c0675d008fa39388f4529184f484ffd8
Svetlozar Todorov <svetlik@gmail.com>
2018-09-08 17:00:04 +0300

    Detect strikes and spares

--- app/models/game.rb
+++ app/models/game.rb
@@ -15,36 +15,47 @@ class Game < ApplicationRecord
     if game_over?
       raise Exception.new('game over')
     else
-      add_score_to_frame(throw_score)
-      add_to_overall_score(throw_score)
+      game_score(throw_score)
     end
+  end
+
+  def game_score(throw_score)
+    add_score_to_frame(throw_score)
+    add_to_overall_score(throw_score)
   end
 
   def increase_throw_counter
     if self.throw_counter == 2
       self.throw_counter = 1
-      move_frame
+      move_frame(1)
     else
       self.throw_counter += 1
     end
     self.save
   end
 
-  def move_frame
-    self.current_frame += 1
+  def move_frame(num)
+    self.current_frame += num
     self.save
   end
 
   def cur_frame
-    frames[current_frame]
+    self.frames[current_frame]
   end
 
   def add_score_to_frame(throw_score)
     increase_throw_counter
     if cur_frame[0].nil?
       cur_frame[0] = throw_score.to_i
+      if cur_frame[0].to_i == 10
+        puts 'strike'
+        increase_throw_counter
+      end
     else
       cur_frame[1] = throw_score.to_i
+      if cur_frame[0].to_i + cur_frame[1].to_i == 10
+        puts 'spare'
+      end
     end
   end
 
